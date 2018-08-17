a5923a9d2d7a3dc90e415b6715774593d1299d16
Marco Poletto <marco.poletto@magairports.com>
2018-08-17 15:46:04 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostList :posts="loadedPosts"/>
+    </section>
+  </div>
+</template>
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+  components: {
+    PostList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {id: '1', title: 'First Post', previewText: 'This is my first post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'},
+          {id: '2', title: 'Second Post', previewText: 'This is my second post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'}
+        ]
+      })    
+    }, 1500)
+  },
+  // data() {
+  //   return {
+  //     loadedPosts: [
+        
+  //     ]
+  //   }
+  // },
+  created() {
+    // setTimeout(() => {
+    //   this.loadedPosts = [
+    //     {id: '1', title: 'First Post', previewText: 'This is my first post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'},
+    //     {id: '2', title: 'Second Post', previewText: 'This is my second post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'}
+    //   ]
+    // }, 1500)
+  }
+}
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~assets/images/main-page-background.jpg')
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+
---
+++ pages/posts/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="posts-page">
+        <PostList :posts="loadedPosts"/>
+    </div>
+</template>
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+    components: {
+        PostList
+    },
+    asyncData(context, callback) {
+        setTimeout(() => {
+        callback(null, {
+            loadedPosts: [
+            {id: '1', title: 'First Post', previewText: 'This is my first post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'},
+            {id: '2', title: 'Second Post', previewText: 'This is my second post', thumbnail: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'}
+            ]
+        })    
+        }, 1500)
+    },
+}
+</script>
+
+<style scoped>
+.posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
